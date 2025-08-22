0e5360831899f798736083edf081fcbbbd04aa34
unknown <matiasaguacho@gmail.com>
2025-08-22 18:29:03 -0400

    Add createsu command for automatic superuser creation

--- core/management/commands/createsu.py
+++ core/management/commands/createsu.py
@@ -7,9 +7,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "treering")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "treering@tecnologyman.cl")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "treering123")
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
