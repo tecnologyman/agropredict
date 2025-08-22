60e7f2da5d84d727b8556138f49281fa7f9001f9
unknown <matiasaguacho@gmail.com>
2025-08-22 18:41:21 -0400

    Remove Dockerfile; add Procfile and createsu command

--- core/management/commands/createsu.py
+++ core/management/commands/createsu.py
@@ -3,16 +3,14 @@ from django.contrib.auth import get_user_model
 import os
 
 class Command(BaseCommand):
-    help = "Crea un superusuario automáticamente si no existe"
-
+    help = "Crea un superusuario si no existe (usa DJANGO_SUPERUSER_*)"
     def handle(self, *args, **kwargs):
         User = get_user_model()
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "treering")
         email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "treering@tecnologyman.cl")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "treering123")
-
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "Tecnologyman01")
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f"Superusuario {username} creado"))
+            self.stdout.write(self.style.SUCCESS(f"Superusuario '{username}' creado"))
         else:
-            self.stdout.write(self.style.WARNING(f"Superusuario {username} ya existe"))
+            self.stdout.write(self.style.WARNING(f"Superusuario '{username}' ya existe"))
